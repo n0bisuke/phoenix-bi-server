bd05ad2dad1b30f38cece242de5bb132d5d5d31e
のびすけ <rsksound@gmail.com>
2017-12-11 15:01:31 +0900

    license

--- assets/vendor/spread/js/license.js
+++
@@ -1,7 +0,0 @@
-//Please provide valid license key.
-GC.Spread.Views.LicenseKey = 'ds-playground.japaneast.cloudapp.azure.com,415471462756222#A0yymTN7IjN4EzN4UTM4IiOiQWSiwSflNHbhZmOiI7ckJye0ICbuFkI1pjIEJCLi4TPnJ7ZM9ENadWYNlUcwoWauJERKBjdXdlSIdjS5pUQjtEZIV6cjZDcPRzMptkNwFWNRJUWRRzU9k7a6AXZ7QGNtpHUxIWc5NlUZJ6QHxERiJlYXR4MvhmI0IyUiwSNzQjNzYDO9ITM0IicfJye&Qf35VfiEzRwEkI0IyQiwiIwEjL6ByUKBCZhVmcwNlI0IiTis7W0ICZyBlIsIiM5EDN7ADIwEjMxcTMwIjI0ICdyNkIsICOwIDM8EDMyIiOiAHeFJCLi46bj9SZyVneh9CcwFGZ53Gbj9CdzFWZuFGchpmLk9WdvJ7Z9FGbw5yckJiOiMXbEJCLi2WakVHdzR7bkJiOiEmTDJCiilj';
-
-//Set GC.Spread.Sheets.LicenseKey becuase there are some demos use GC.Spread.Sheets components
-// if (GC.Spread.Sheets) {
-//     GC.Spread.Sheets.LicenseKey = 'Your key here';
-// }
